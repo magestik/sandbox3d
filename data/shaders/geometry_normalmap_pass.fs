b64875c43040368f9def1e69f400e5d754593920
magestik <bastien@magestik.fr>
2014-12-01 18:31:28 +0100

    Fixed Normap Mapping fragment shader

--- data/shaders/geometry_normalmap_pass.fs
+++ data/shaders/geometry_normalmap_pass.fs
@@ -3,26 +3,21 @@
 uniform sampler2D normalMap;
 
 in vec2 outTexCoord;
-in vec4 outTangentSpace;
-
 in vec3 outTangent;
 in vec3 outBiTangent;
-in vec3 outNormal;
-
-in vec4 outQuaternion;
 
 out vec3 fbNormal;
 
 void main(void)
 {
-        vec3 normal_tangentspace = normalize(texture(normalMap, outTexCoord).rgb * 2.0 - 1.0);
+		vec3 normal_tangentspace = normalize(texture(normalMap, outTexCoord).rgb * 2.0 - 1.0);
 
-        vec3 tangent    = normalize(outTangent);
-        vec3 bitangent  = normalize(outBiTangent);
-        vec3 normal     = cross(tangent, bitangent);
+		vec3 tangent    = normalize(outTangent);
+		vec3 bitangent  = normalize(outBiTangent);
+		vec3 normal     = cross(bitangent, tangent);
 
-        mat3x3 tangent_space = mat3x3(tangent, bitangent, normal);
+		mat3x3 tangent_space = mat3x3(tangent, bitangent, normal);
 
-        fbNormal = normalize(outNormal) * 0.5 + 0.5;
+		fbNormal = normalize(normal_tangentspace * tangent_space) * 0.5 + 0.5;
 }
 
