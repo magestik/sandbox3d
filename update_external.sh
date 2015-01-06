15fc18f101923829a63b199121d36ad4ca00ae81
magestik <bastien@magestik.fr>
2015-01-06 19:21:52 +0100

    removed curves (unused)

--- update_external.sh
+++ update_external.sh
@@ -5,11 +5,6 @@ git clone https://github.com/magestik/linear-algebra.git
 rm -rf linear-algebra/.git
 rm -rf linear-algebra/.gitignore
 
-rm -rf curves
-git clone https://github.com/magestik/curves.git
-rm -rf curves/.git
-rm -rf curves/.gitignore
-
 rm -rf assimp
 git clone https://github.com/assimp/assimp.git
 rm -rf assimp/.git
