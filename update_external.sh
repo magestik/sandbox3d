989178d78b30d097b98d87705693b3794caf015a
magestik <bastien@magestik.fr>
2015-01-24 20:07:27 +0100

    updated external-dependancies-downloader script

--- update_external.sh
+++ update_external.sh
@@ -4,8 +4,3 @@ rm -rf linear-algebra
 git clone https://github.com/magestik/linear-algebra.git
 rm -rf linear-algebra/.git
 rm -rf linear-algebra/.gitignore
-
-rm -rf assimp
-git clone https://github.com/assimp/assimp.git
-rm -rf assimp/.git
-rm -rf assimp/.gitignore
