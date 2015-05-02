02681ea6aad956275bbf0380859c39f2da67fe68
magestik <bastien@magestik.fr>
2015-05-02 18:06:26 +0200

    Removed GL3/gl3.h (now use GL/glcorearb.h)

--- include/GPU_mem.h
+++ include/GPU_mem.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <GL3/gl3.h>
+#include <GL/glcorearb.h>
 #include <GPU.h>
 #include <string.h>
 
@@ -16,9 +16,9 @@ namespace GPU
 template<GLenum T>
 inline void realloc(const Buffer<T> & buffer, size_t s, const void * pData = nullptr)
 {
-    glBindBuffer(T, buffer.GetObject());
-    glBufferData(T, s, pData, GL_STATIC_DRAW);
-    glBindBuffer(T, 0);
+	glBindBuffer(T, buffer.GetObject());
+	glBufferData(T, s, pData, GL_STATIC_DRAW);
+	glBindBuffer(T, 0);
 }
 
 /**
@@ -31,9 +31,9 @@ inline void realloc(const Buffer<T> & buffer, size_t s, const void * pData = nul
 template<GLenum T>
 inline void realloc(const Buffer<T> & buffer, size_t s, GLenum usage, const void * pData = nullptr)
 {
-    glBindBuffer(T, buffer.GetObject());
-    glBufferData(T, s, pData, usage);
-    glBindBuffer(T, 0);
+	glBindBuffer(T, buffer.GetObject());
+	glBufferData(T, s, pData, usage);
+	glBindBuffer(T, 0);
 }
 
 /**
@@ -44,10 +44,10 @@ inline void realloc(const Buffer<T> & buffer, size_t s, GLenum usage, const void
 template<GLenum T>
 void * mmap(const Buffer<T> & buffer, GLuint access)
 {
-    glBindBuffer(T, buffer.GetObject());
-    void * ptr = glMapBuffer(T, access);
-    glBindBuffer(T, 0);
-    return(ptr);
+	glBindBuffer(T, buffer.GetObject());
+	void * ptr = glMapBuffer(T, access);
+	glBindBuffer(T, 0);
+	return(ptr);
 }
 
 /**
@@ -58,9 +58,9 @@ void * mmap(const Buffer<T> & buffer, GLuint access)
 template<GLenum T>
 void munmap(const Buffer<T> & buffer)
 {
-    glBindBuffer(T, buffer.GetObject());
-    glUnmapBuffer(T);
-    glBindBuffer(T, 0);
+	glBindBuffer(T, buffer.GetObject());
+	glUnmapBuffer(T);
+	glBindBuffer(T, 0);
 }
 
 /**
@@ -71,11 +71,11 @@ void munmap(const Buffer<T> & buffer)
 template<GLenum T>
 void memcpy(const Buffer<T> & buffer, const void * pData, size_t s)
 {
-    glBindBuffer(T, buffer.GetObject());
-    void * ptr = glMapBufferRange(T, 0, s, GL_MAP_WRITE_BIT);
-    ::memcpy(ptr, pData, s);
-    glUnmapBuffer(T);
-    glBindBuffer(T, 0);
+	glBindBuffer(T, buffer.GetObject());
+	void * ptr = glMapBufferRange(T, 0, s, GL_MAP_WRITE_BIT);
+	::memcpy(ptr, pData, s);
+	glUnmapBuffer(T);
+	glBindBuffer(T, 0);
 }
 
 /**
@@ -86,11 +86,11 @@ void memcpy(const Buffer<T> & buffer, const void * pData, size_t s)
 template<GLenum T>
 void memcpy(void * pData, const Buffer<T> & buffer, size_t s)
 {
-    glBindBuffer(T, buffer.GetObject());
-    const void * ptr = glMapBufferRange(T, 0, s, GL_MAP_READ_BIT);
-    ::memcpy(pData, ptr, s);
-    glUnmapBuffer(T);
-    glBindBuffer(T, 0);
+	glBindBuffer(T, buffer.GetObject());
+	const void * ptr = glMapBufferRange(T, 0, s, GL_MAP_READ_BIT);
+	::memcpy(pData, ptr, s);
+	glUnmapBuffer(T);
+	glBindBuffer(T, 0);
 }
 
 }
