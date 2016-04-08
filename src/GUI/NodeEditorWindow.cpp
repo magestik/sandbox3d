06acd3140625acebd33fa05aae26c3f777d996c8
magestik <bastien@magestik.fr>
2016-04-09 01:32:29 +0200

    Added context menus to create nodes

--- src/GUI/NodeEditorWindow.cpp
+++ src/GUI/NodeEditorWindow.cpp
@@ -3,6 +3,9 @@
 
 #include <QVBoxLayout>
 #include <QStatusBar>
+#include <QMenuBar>
+#include <QContextMenuEvent>
+#include <QAction>
 
 #include <graphicsnodescene.hpp>
 #include <graphicsnodeview.hpp>
@@ -10,6 +13,8 @@
 #include <graphicsnode.hpp>
 #include <graphicsbezieredge.hpp>
 #include <qobjectnode.hpp>
+
+#include <assert.h>
 
 /**
  * @brief NodeEditorWindow::NodeEditorWindow
@@ -21,28 +26,47 @@ NodeEditorWindow::NodeEditorWindow(QWidget * pParent)
 , m_pView(nullptr)
 , m_pScene(nullptr)
 {
+	ui->setupUi(this);
+
 	setWindowFlags(Qt::Window | Qt::CustomizeWindowHint | Qt::WindowTitleHint | Qt::WindowSystemMenuHint | Qt::WindowCloseButtonHint | Qt::WindowMinMaxButtonsHint);
 
 	setMinimumWidth(640);
 	setMinimumHeight(480);
 
-	// create and configure scene
-	m_pScene = new GraphicsNodeScene(this);
-	m_pScene->setSceneRect(-32000, -32000, 64000, 64000);
+	// Menu Bar
+	{
+		m_pMenuBar = new QMenuBar;
+		ui->layout->addWidget(m_pMenuBar);
+	}
 
-	// view setup
-	m_pView = new GraphicsNodeView(this);
-	m_pView->setScene(m_pScene);
+	// Node Widget
+	{
+		// create and configure scene
+		m_pScene = new GraphicsNodeScene(this);
+		m_pScene->setSceneRect(-32000, -32000, 64000, 64000);
 
+		// view setup
+		m_pView = new GraphicsNodeView(this);
+		m_pView->setScene(m_pScene);
 
-	QVBoxLayout * pLayout = new QVBoxLayout(this);
-	pLayout->setMargin(0);
-	pLayout->addWidget(m_pView);
+		ui->layout->addWidget(m_pView);
+	}
 
-	QStatusBar * statusBar = new QStatusBar;
-	pLayout->addWidget(statusBar);
+	// Status Bar
+	{
+		m_pStatusBar = new QStatusBar;
+		ui->layout->addWidget(m_pStatusBar);
+	}
 
-	setLayout(pLayout);
+	// Centext menu
+	{
+		m_pContextMenuScene = new QMenu(this);
+		m_pContextMenuScene->addAction(ui->actionAddNode);
+
+		m_pContextMenuNode = new QMenu(this);
+		m_pContextMenuNode->addAction(ui->actionAddInput);
+		m_pContextMenuNode->addAction(ui->actionAddOutput);
+	}
 }
 
 /**
@@ -60,4 +84,92 @@ NodeEditorWindow::~NodeEditorWindow()
 void NodeEditorWindow::closeEvent(QCloseEvent * pEvent)
 {
 	// nothing here
+}
+
+/**
+ * @brief NodeEditorWindow::contextMenuEvent
+ * @param event
+ */
+void NodeEditorWindow::contextMenuEvent(QContextMenuEvent * pEvent)
+{
+	QGraphicsItem * item = m_pView->itemAt(pEvent->pos());
+
+	m_pScene->clearSelection();
+
+	if (item)
+	{
+		switch (item->type())
+		{
+			case GraphicsNodeItemTypes::TypeNode:
+			{
+				item->setSelected(true);
+
+				m_pContextMenuNode->exec(pEvent->globalPos());
+			}
+			break;
+		}
+	}
+	else
+	{
+		m_pContextMenuScene->exec(pEvent->globalPos());
+	}
+}
+
+/**
+ * @brief NodeEditorWindow::on_actionAddNode_triggered
+ */
+void NodeEditorWindow::on_actionAddNode_triggered()
+{
+	auto n = new GraphicsNode();
+
+	n->setTitle(QString("Node"));
+
+	m_pScene->addItem(n);
+}
+
+/**
+ * @brief NodeEditorWindow::on_actionAddInput_triggered
+ */
+void NodeEditorWindow::on_actionAddInput_triggered(void)
+{
+	QList<QGraphicsItem *> list = m_pScene->selectedItems();
+	assert(list.count() == 1);
+
+	QGraphicsItem * item = list.at(0);
+	assert(item->type() == GraphicsNodeItemTypes::TypeNode);
+
+	GraphicsNode * node = static_cast<GraphicsNode*>(item);
+	node->add_sink("input");
+}
+
+/**
+ * @brief NodeEditorWindow::on_actionAddOutput_triggered
+ */
+void NodeEditorWindow::on_actionAddOutput_triggered(void)
+{
+	QList<QGraphicsItem *> list = m_pScene->selectedItems();
+	assert(list.count() == 1);
+
+	QGraphicsItem * item = list.at(0);
+	assert(item->type() == GraphicsNodeItemTypes::TypeNode);
+
+	GraphicsNode * node = static_cast<GraphicsNode*>(item);
+	node->add_source("output");
+}
+
+/**
+ * @brief NodeEditorWindow::on_actionDelete_triggered
+ */
+void NodeEditorWindow::on_actionDelete_triggered(void)
+{
+	QList<QGraphicsItem *> list = m_pScene->selectedItems();
+
+	for (QGraphicsItem * item : list)
+	{
+		if (item->type() == GraphicsNodeItemTypes::TypeNode)
+		{
+			m_pScene->removeItem(item);
+			delete item;
+		}
+	}
 }
--- src/GUI/NodeEditorWindow.h
+++ src/GUI/NodeEditorWindow.h
@@ -2,6 +2,10 @@
 
 #include <QDialog>
 
+class QMenuBar;
+class QStatusBar;
+class QMenu;
+
 class GraphicsNodeView;
 class GraphicsNodeScene;
 
@@ -18,7 +22,8 @@ public:
 	explicit	NodeEditorWindow		(QWidget * parent = 0);
 	virtual		~NodeEditorWindow		(void);
 
-	void		closeEvent				(QCloseEvent * event);
+	void		closeEvent				(QCloseEvent * event) Q_DECL_OVERRIDE;
+	void		contextMenuEvent		(QContextMenuEvent * event) Q_DECL_OVERRIDE;
 
 private:
 
@@ -26,6 +31,22 @@ private:
 
 protected:
 
+	QMenuBar *				m_pMenuBar;
+	QStatusBar *			m_pStatusBar;
+
 	GraphicsNodeView *		m_pView;
 	GraphicsNodeScene *		m_pScene;
+
+	QMenu *					m_pContextMenuScene;
+	QMenu *					m_pContextMenuNode;
+
+private slots:
+
+	void on_actionAddNode_triggered();
+
+	void on_actionAddInput_triggered();
+	void on_actionAddOutput_triggered();
+
+	void on_actionDelete_triggered();
+
 };
