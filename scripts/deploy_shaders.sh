a7b6fed4743bab28629131ec830bfd5f06928151
magestik <bastien@magestik.fr>
2017-03-12 02:45:53 +0100

    Fixed shader deployement

--- scripts/deploy_shaders.sh
+++ scripts/deploy_shaders.sh
@@ -13,7 +13,7 @@ for f in $FRAG $VERT $GEOM
 do
 	# TODO : use the shaderc instead
 	name=$(basename "$f")
-	glslc -std=330 -E $f -I src/Rendering/Shader/shaders/ | sed 's/^#extension GL_GOOGLE_include_directive : enable$/ /' | sed 's/^#line [0-9]* .*$/ /' > $DEST/$name
+	/mnt/workspace/ShadersCompilers/shaderc/build/glslc/glslc -std=330 -E $f -I src/Rendering/Shader/shaders/ | sed 's/^#extension GL_GOOGLE_include_directive : enable$/ /' | sed 's/^#line [0-9]* .*$/ /' > $DEST/$name
 done
 
 #cd build
