0a99ab9e2030e6153c118b7f4a8b3111446b8e5d
magestik <bastien@magestik.fr>
2018-02-18 00:42:17 +0100

    Removed glBreak (not useful anymore in this repository)

--- external/glBreak/src/functions.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-extern void glDebugMessageControl(GLenum source, GLenum type, GLenum severity, GLsizei count, const GLuint * ids, GLboolean enabled);
-extern void glDebugMessageInsert(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar * buf);
-extern void glDebugMessageCallback(GLDEBUGPROC callback, const void * userParam);
-extern GLuint glGetDebugMessageLog(GLuint count, GLsizei bufSize, GLenum * sources, GLenum * types, GLuint * ids, GLenum *severities, GLsizei *lengths, GLchar * messageLog);
-extern void glPushDebugGroup(GLenum source, GLuint id, GLsizei length, const GLchar * message);
-extern void glPopDebugGroup(void);
