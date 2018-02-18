c6ccd6049febc19543762197764c9c3680eef3c8
magestik <bastien@magestik.fr>
2018-02-18 19:12:40 +0100

    Cleaned deploy_shaders.sh script

--- scripts/deploy_shaders.sh
+++ scripts/deploy_shaders.sh
@@ -2,7 +2,6 @@
 
 mkdir -p data/shaders/
 
-
 FRAG=src/Rendering/Shader/shaders/*.frag
 VERT=src/Rendering/Shader/shaders/*.vert
 GEOM=src/Rendering/Shader/shaders/*.geom
@@ -11,11 +10,10 @@ DEST=data/shaders/
 
 for f in $FRAG $VERT $GEOM
 do
-	# TODO : use the shaderc instead
-	name=$(basename "$f")
-	/mnt/workspace/ShadersCompilers/shaderc/build/glslc/glslc -std=330 -E $f -I src/Rendering/Shader/shaders/ | sed 's/^#extension GL_GOOGLE_include_directive : enable$/ /' | sed 's/^#line [0-9]* .*$/ /' > $DEST/$name
+	if [ -f $f ]; then
+		# TODO : use the shaderc instead
+		name=$(basename "$f")
+		/mnt/workspace/ShadersCompilers/shaderc/build/glslc/glslc -std=330 -E $f -I src/Rendering/Shader/shaders/ | sed 's/^#extension GL_GOOGLE_include_directive : enable$/ /' | sed 's/^#line [0-9]* .*$/ /' > $DEST/$name
+	fi
 done
-
-#cd build
-#ctest check_shaders -VV
 
